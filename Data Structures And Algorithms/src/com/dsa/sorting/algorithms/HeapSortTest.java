2348bbeaf28e4ddb8fa1ad841658a95bdf344381
SandipCodes <sandipsh321@gmail.com>
2019-10-07 00:13:43 +0530

    heap sort

--- Data Structures And Algorithms/src/com/dsa/sorting/algorithms/HeapSortTest.java
+++ Data Structures And Algorithms/src/com/dsa/sorting/algorithms/HeapSortTest.java
@@ -6,43 +6,55 @@ public class HeapSortTest {
 
 	public static void main(String[] args) {
 
-		int input[] = new int[] { 5, 2, 7, 3, 6, 1, 4 };
+
+		int input[] = new int[] { 8,15,10,16,50,20,30,60 };
+//		int input[] = new int[] {1, 10, 2, 3, 4, 1, 2, 100, 23, 2}; 
 
 		int len = input.length;
-		int temp, j, k, i,root;
-		int heap[] = new int[len];
 
-		System.out.println("input:" + Arrays.toString(input));
-          heap[0]=input[0];
-          
-		for (i = 1; i < len; i++) {
-
-			heap[i] = input[i];
-			
-			// maintaining the max heap tree
-			j = i;
-			while (j >= 0) {
-
-				if(j <=2) {
-					root=0;
-				}
-				else {
-					root= j/2-1;
-				 }
-				
-				if (heap[j] > heap[root]) {
-					temp = heap[j];
-					heap[j] = heap[root];
-					heap[root] = temp;
-				} // if
-				else {
-					break;
-				}
-				j = j / 2;
-				
-			} // while
-
-			System.out.println("itr:" + i + "  " + Arrays.toString(input));
-		} // for
+		System.out.println("Input:"+Arrays.toString(input));
+		heapSort(input,len);
+		System.out.println("Output:"+Arrays.toString(input));
 	}// main()
+	
+	private static void heapSort(int input[],int len) {
+		
+		int i,temp;
+		
+		for(i=len/2-1;i>=0;i--) {
+			heapify(input,len,i);
+		}//for
+		
+		for(i=len-1;i>=0;i--) {
+			temp=input[0];
+			input[0]=input[i];
+			input[i]=temp;
+			heapify(input,i,0);
+		}//for
+		
+	}//heapSort()
+	
+	private static void heapify(int input[],int len,int i) {
+		int temp;
+		int largest=i;
+		int left=2*i+1;
+		int right=2*i+2;
+		
+		if(left < len && input[left] > input[largest])
+			largest=left;
+
+		if(right < len && input[right] > input[largest])
+			largest=right;
+		
+		if(largest !=i) {
+			
+			temp=input[i];
+			input[i]=input[largest];
+			input[largest]=temp;
+			
+			heapify(input,len,largest);
+		}//if
+		
+	}//heapify()
+	
 }// class
